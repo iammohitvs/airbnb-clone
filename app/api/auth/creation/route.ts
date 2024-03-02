92a1d845e2d4eb1bf9cdaec790d229e738d38c8e
Mohit V S <iammohitvs@Mohits-MacBook-Air.local>
2024-03-03 00:37:12 +0530

    changed urls

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,5 +34,5 @@ export async function GET() {
         });
     }
 
-    return NextResponse.redirect("http://localhost:3000");
+    return NextResponse.redirect("http://airbnb-clone-mohitvs.vercel.app");
 }
